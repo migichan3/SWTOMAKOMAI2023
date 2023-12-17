b026b9b9e54a87a24e9c0ddaff4c537406fbb7e6
migichan3 <miginamix188@gmail.com>
2023-12-17 12:33:39 +0900

    完成

--- detail.js
+++ detail.js
@@ -1,20 +1,26 @@
 const productsData = [
     {
         "productName": "商品名1",
-        "review": "レビュー1",
-        "comment": "口コミ1",
+        "price": "1,000円",
+        "material": "PH調整剤，アミノ酸",
+        "review": "めちゃおいしい",
+        "comment": "4.0/5.0",
         "img": "img/prod1.png"
     },
     {
         "productName": "商品名2",
-        "review": "レビュー2",
-        "comment": "口コミ2",
+        "price": "3,000円",
+        "material": "PH調整剤",
+        "review": "体に良い",
+        "comment": "4.2/5.0",
         "img": "img/prod1.png"
     },
     {
         "productName": "商品名3",
-        "review": "レビュー3",
-        "comment": "口コミ3",
+        "price": "1500円",
+        "material": "アミノ酸，増粘剤",
+        "review": "またかいたい！",
+        "comment": "3.8/5.0",
         "img": "img/prod1.png"
     }
 ];
@@ -24,6 +30,8 @@ function loadProductData() {
 
     productsData.forEach(product => {
         content += `<h3>商品名：${product.productName}</h3>`;
+        content += `<p>価格：${product.price}</p>`;
+        content += `<p>原材料：${product.material}</p>`;
         content += `<p>口コミ：${product.comment}</p>`;
         content += `<p>レビュー：${product.review}</p>`;
         content += `<img src="${product.img}" alt="${product.productName}" style="width: 300px;">`;
